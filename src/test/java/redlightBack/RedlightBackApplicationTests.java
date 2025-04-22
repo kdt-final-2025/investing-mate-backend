ea9109068387065760e4eab096950bf2ebf9c58d
youn <tty7797@naver.com>
2025-04-22 17:02:09 +0900

    feat : 환경설정 & 도메인별 나누기

---
+++ src/test/java/redlightBack/RedlightBackApplicationTests.java
@@ -0,0 +1,13 @@
+package redlightBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedlightBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
