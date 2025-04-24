42798bf4feadc2fc0d511328c1b1dbc8b22c00f1
HYOJIN <hjhwang08@naver.com>
2025-04-24 13:54:07 +0900

    fix: 빌드 에러 해결 (#5)
    * fix: 로컬 DB 설정 분리
    
    * fix: @EnableJpaAuditing 중복 제거
    
    * fix: H2 추가, ApplicationTests에 @ActiveProfiles("test") 추가
    
    * fix: test 설정에 jwt.secret 추가

--- src/test/java/redlightBack/ApplicationTests.java
+++ src/test/java/redlightBack/ApplicationTests.java
@@ -2,9 +2,11 @@ package redlightBack;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
-class RedlightBackApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
