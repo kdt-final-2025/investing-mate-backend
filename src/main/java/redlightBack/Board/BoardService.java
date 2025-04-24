627fd6f6f8bbf7829723c6dfc8da210af3fad23c
HYOJIN <hjhwang08@naver.com>
2025-04-24 13:36:58 +0900

    temp

--- src/main/java/redlightBack/Board/BoardService.java
+++ src/main/java/redlightBack/Board/BoardService.java
@@ -14,7 +14,12 @@ public class BoardService {
         this.boardRepository = boardRepository;
     }
 
+//    public BoardResponse create (String userId){
+//        Board board = new Board()
+//    }
 
+
+    //게시판 목록조회
     public List<BoardResponse> getBoardList (){
         return boardRepository.findAll().stream().map(
                 board -> new BoardResponse(board.getId(), board.getBoardName(), board.postCount)
