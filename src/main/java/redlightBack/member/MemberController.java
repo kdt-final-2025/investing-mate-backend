874c9f19ab4ac7860495b49d4e16eeb81d064dec
Sungrak Kim <kimsungrak00@gmail.com>
2025-04-28 02:43:24 +0900

    fix: MemberController 설명 수정

--- src/main/java/redlightBack/member/MemberController.java
+++ src/main/java/redlightBack/member/MemberController.java
@@ -11,11 +11,9 @@ public class MemberController {
 
     private final MemberService memberService;
 
-    // 생성자 주입만! Lombok @RequiredArgsConstructor 불필요
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
-
 
     // POST /members/me
     // - Jwt를 서비스에 넘겨서 upsert 수행
@@ -24,7 +22,6 @@ public class MemberController {
     public MemberResponseDto provisionUser(@AuthenticationPrincipal Jwt jwt) {
         return memberService.provisionUserFromJwt(jwt);
     }
-
 
     // PATCH /members/{userId}/promote
     // 일반유저를 기자로 바꿔주는 로직
