9738a1bca00f9b8f2e596057f8e55def7f3762a4
Kimmy <kimsungrak00@gmail.com>
2025-04-28 14:04:35 +0900

    refactor(SecurityConfig): SecurityFilter 제거

--- src/main/java/redlightBack/common/SecurityConfig.java
+++ src/main/java/redlightBack/common/SecurityConfig.java
@@ -1,17 +1,12 @@
 package redlightBack.common;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
 import com.nimbusds.jose.JWSAlgorithm;
 import com.nimbusds.jose.jwk.OctetSequenceKey;
-
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.oauth2.jwt.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
@@ -31,32 +26,5 @@ public class SecurityConfig {
         // HS256 검증기 생성
         NimbusJwtDecoder decoder = NimbusJwtDecoder.withSecretKey(key).build();
         return decoder;
-    }
-
-    @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                // (1) CORS 활성화
-                .cors(Customizer -> {})
-
-                // (2) CSRF 끄기
-                .csrf(AbstractHttpConfigurer::disable)
-
-                // (3) 기본 인증/Form 로그인 끄기
-                .httpBasic(AbstractHttpConfigurer::disable)
-                .formLogin(AbstractHttpConfigurer::disable)
-
-                // (4) 인가 설정
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/members/me").authenticated()
-                        .anyRequest().permitAll()
-                )
-
-                // (5) JWT Resource Server 설정
-                .oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(Customizer.withDefaults())
-                );
-
-        return http.build();
     }
 }
