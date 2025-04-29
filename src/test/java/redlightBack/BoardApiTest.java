5511f2c949e484feef4490dc5925b44fe02aa13a
HYOJIN <hjhwang08@naver.com>
2025-04-29 14:42:50 +0900

    test: 좋아요/좋아요 취소, 사용자가 좋아요 한 post 목록 보기 테스트 & 통과

--- src/test/java/redlightBack/BoardApiTest.java
+++ src/test/java/redlightBack/BoardApiTest.java
@@ -14,6 +14,7 @@ import redlightBack.Post.Enum.Direction;
 import redlightBack.Post.Enum.SortBy;
 
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -37,7 +38,7 @@ public class BoardApiTest extends AcceptanceTest {
     public void Board_Create_test(){
 
         //테스트용 토큰
-        String token = generateTestToken();
+        String token = generateTestTokens("user1");
 
         BoardResponse response = RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
@@ -85,7 +86,7 @@ public class BoardApiTest extends AcceptanceTest {
     @Test
     public void 게시글_생성_테스트(){
 
-        String token = generateTestToken();
+        String token = generateTestTokens("user1");
         BoardResponse testBoard = createBoard("자유게시판");
         Long boardId = testBoard.id();
 
@@ -141,7 +142,7 @@ public class BoardApiTest extends AcceptanceTest {
         PostResponse post = createPost(new CreatePostRequest(boardId, "제목", "내용", List.of("img1", "img2", "img3")));
         Long postId = post.id();
 
-        String token = generateTestToken();
+        String token = generateTestTokens("user1");
 
         PostResponse postResponse = RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
@@ -169,7 +170,7 @@ public class BoardApiTest extends AcceptanceTest {
         PostResponse post = createPost(new CreatePostRequest(boardId, "제목", "내용", List.of("img1", "img2", "img3")));
         Long postId = post.id();
 
-        String token = generateTestToken();
+        String token = generateTestTokens("user1");
 
         DeletePostResponse deletePostResponse = RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
@@ -315,6 +316,268 @@ public class BoardApiTest extends AcceptanceTest {
 
     }
 
+    @DisplayName("좋아요 테스트")
+    @Test
+    public void 좋아요_테스트 (){
+        BoardResponse board = createBoard("자유게시판");
+        Long boardId = board.id();
+
+        PostResponse post = createPost(new CreatePostRequest(boardId, "제목1", "내용1", List.of("img1", "img2", "img3")));
+        Long postId = post.id();
+
+        String token = generateTestTokens("user1");
+
+        PostLikeResponse postLikeResponse = RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .pathParam("postId", postId)
+                .header("Authorization", "Bearer " + token)
+                .when()
+                .post("/posts/{postId}/like")
+                .then().log().all()
+                .statusCode(200)
+                .extract()
+                .as(PostLikeResponse.class);
+
+        assertThat(postLikeResponse.likeCount()).isEqualTo(1);
+    }
+
+    @DisplayName("좋아요 취소 테스트")
+    @Test
+    public void 좋아요_취소_테스트 (){
+        BoardResponse board = createBoard("자유게시판");
+        Long boardId = board.id();
+
+        PostResponse post = createPost(new CreatePostRequest(boardId, "제목1", "내용1", List.of("img1", "img2", "img3")));
+        Long postId = post.id();
+
+        String token = generateTestTokens("user1");
+        toggleLike(postId, token);
+        PostLikeResponse postLikeResponse = toggleLike(postId, token);
+
+        assertThat(postLikeResponse.likeCount()).isEqualTo(0);
+        assertThat(postLikeResponse.liked()).isEqualTo(false);
+    }
+
+    @DisplayName("여러 사용자의 좋아요 테스트")
+    @Test
+    public void 여러_사용자의_좋아요_테스트 (){
+        BoardResponse board = createBoard("자유게시판");
+        Long boardId = board.id();
+
+        PostResponse post = createPost(new CreatePostRequest(boardId, "제목1", "내용1", List.of("img1", "img2", "img3")));
+        Long postId = post.id();
+
+        toggleLike(postId, generateTestTokens("user1"));
+        toggleLike(postId, generateTestTokens("user2"));
+        toggleLike(postId, generateTestTokens("user3"));
+        toggleLike(postId, generateTestTokens("user4"));
+        toggleLike(postId, generateTestTokens("user5"));
+        PostLikeResponse postLikeResponse = toggleLike(postId, generateTestTokens("user6"));
+
+        PostResponse updatedPost = getDetail(postId);
+
+        assertThat(postLikeResponse.likeCount()).isEqualTo(6);
+        assertThat(updatedPost.likeCount()).isEqualTo(6);
+    }
+
+    @DisplayName("여러 사용자의 좋아요 테스트2")
+    @Test
+    public void 여러_사용자의_좋아요_테스트2 (){
+        BoardResponse board = createBoard("자유게시판");
+        Long boardId = board.id();
+
+        PostResponse post = createPost(new CreatePostRequest(boardId, "제목1", "내용1", List.of("img1", "img2", "img3")));
+        Long postId = post.id();
+
+        toggleLike(postId, generateTestTokens("user1"));
+        toggleLike(postId, generateTestTokens("user1"));
+
+        toggleLike(postId, generateTestTokens("user2"));
+        toggleLike(postId, generateTestTokens("user2"));
+
+        toggleLike(postId, generateTestTokens("user3"));
+        toggleLike(postId, generateTestTokens("user3"));
+
+        toggleLike(postId, generateTestTokens("user4"));
+        toggleLike(postId, generateTestTokens("user5"));
+        PostLikeResponse postLikeResponse = toggleLike(postId, generateTestTokens("user6"));
+
+        PostResponse updatedPost = getDetail(postId);
+
+        assertThat(postLikeResponse.likeCount()).isEqualTo(3);
+        assertThat(updatedPost.likeCount()).isEqualTo(3);
+    }
+
+    @DisplayName("좋아요순으로 목록 조회 테스트")
+    @Test
+    public void 좋아요순으로_목록_조회_테스트 (){
+        BoardResponse board = createBoard("자유게시판");
+        Long boardId = board.id();
+
+        PostResponse post1 = createPost(new CreatePostRequest(boardId, "제목1", "내용1", List.of("img1", "img2", "img3")));
+        PostResponse post2 = createPost(new CreatePostRequest(boardId, "제목2", "내용2", List.of("img1", "img2", "img3")));
+        PostResponse post3 = createPost(new CreatePostRequest(boardId, "제목3", "내용3", List.of("img1", "img2", "img3")));
+        PostResponse post4 = createPost(new CreatePostRequest(boardId, "제목4", "내용4", List.of("img1", "img2", "img3")));
+        PostResponse post5 = createPost(new CreatePostRequest(boardId, "제목5", "내용5", List.of("img1", "img2", "img3")));
+        PostResponse post6 = createPost(new CreatePostRequest(boardId, "제목6", "내용6", List.of("img1", "img2", "img3")));
+        PostResponse post7 = createPost(new CreatePostRequest(boardId, "제목7", "내용7", List.of("img1", "img2", "img3")));
+        PostResponse post8 = createPost(new CreatePostRequest(boardId, "제목8", "내용8", List.of("img1", "img2", "img3")));
+
+        Long postId1 = post1.id();
+        Long postId2 = post2.id();
+        Long postId3 = post3.id();
+        Long postId4 = post4.id();
+
+        toggleLike(postId3, generateTestTokens("user1"));
+        toggleLike(postId3, generateTestTokens("user2"));
+        toggleLike(postId3, generateTestTokens("user3"));
+        toggleLike(postId3, generateTestTokens("user4"));
+        toggleLike(postId3, generateTestTokens("user5"));
+        toggleLike(postId3, generateTestTokens("user6"));
+
+        toggleLike(postId1, generateTestTokens("user1"));
+        toggleLike(postId1, generateTestTokens("user2"));
+        toggleLike(postId1, generateTestTokens("user3"));
+        toggleLike(postId1, generateTestTokens("user4"));
+        toggleLike(postId1, generateTestTokens("user5"));
+
+        toggleLike(postId2, generateTestTokens("user1"));
+        toggleLike(postId2, generateTestTokens("user2"));
+        toggleLike(postId2, generateTestTokens("user3"));
+
+        toggleLike(postId4, generateTestTokens("user3"));
+
+
+        PostListAndPagingResponse response = RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .queryParam("boardId", boardId)
+                .queryParam("sortBy", SortBy.MOST_LIKED)
+                .when()
+                .get("/posts")
+                .then().log().all()
+                .statusCode(200)
+                .extract()
+                .as(PostListAndPagingResponse.class);
+
+        List<PostListResponse> postListResponses = response.postListResponse();
+
+        assertThat(postListResponses.get(0).postTitle()).isEqualTo("제목3");
+        assertThat(postListResponses.get(1).postTitle()).isEqualTo("제목1");
+        assertThat(postListResponses.get(2).postTitle()).isEqualTo("제목2");
+        assertThat(postListResponses.get(3).postTitle()).isEqualTo("제목4");
+        assertThat(postListResponses.get(4).postTitle()).isEqualTo("제목8");
+        assertThat(postListResponses.get(5).postTitle()).isEqualTo("제목7");
+
+    }
+
+    @DisplayName("좋아요순 오래된 게시물 순서로 목록 조회")
+    @Test
+    public void 좋아요와_오래된_게시물_순서로_목록_조회 (){
+        BoardResponse board = createBoard("자유게시판");
+        Long boardId = board.id();
+
+        PostResponse post1 = createPost(new CreatePostRequest(boardId, "제목1", "내용1", List.of("img1", "img2", "img3")));
+        PostResponse post2 = createPost(new CreatePostRequest(boardId, "제목2", "내용2", List.of("img1", "img2", "img3")));
+        PostResponse post3 = createPost(new CreatePostRequest(boardId, "제목3", "내용3", List.of("img1", "img2", "img3")));
+        PostResponse post4 = createPost(new CreatePostRequest(boardId, "제목4", "내용4", List.of("img1", "img2", "img3")));
+        PostResponse post5 = createPost(new CreatePostRequest(boardId, "제목5", "내용5", List.of("img1", "img2", "img3")));
+        PostResponse post6 = createPost(new CreatePostRequest(boardId, "제목6", "내용6", List.of("img1", "img2", "img3")));
+        PostResponse post7 = createPost(new CreatePostRequest(boardId, "제목7", "내용7", List.of("img1", "img2", "img3")));
+        PostResponse post8 = createPost(new CreatePostRequest(boardId, "제목8", "내용8", List.of("img1", "img2", "img3")));
+
+        Long postId8 = post8.id();
+        Long postId2 = post2.id();
+        Long postId3 = post3.id();
+
+        toggleLike(postId3, generateTestTokens("user1"));
+        toggleLike(postId3, generateTestTokens("user2"));
+        toggleLike(postId3, generateTestTokens("user3"));
+        toggleLike(postId3, generateTestTokens("user4"));
+        toggleLike(postId3, generateTestTokens("user5"));
+        toggleLike(postId3, generateTestTokens("user6"));
+
+        toggleLike(postId8, generateTestTokens("user1"));
+        toggleLike(postId8, generateTestTokens("user2"));
+        toggleLike(postId8, generateTestTokens("user3"));
+        toggleLike(postId8, generateTestTokens("user4"));
+        toggleLike(postId8, generateTestTokens("user5"));
+        toggleLike(postId8, generateTestTokens("user6"));
+
+        toggleLike(postId2, generateTestTokens("user1"));
+        toggleLike(postId2, generateTestTokens("user2"));
+        toggleLike(postId2, generateTestTokens("user3"));
+        toggleLike(postId2, generateTestTokens("user4"));
+        toggleLike(postId2, generateTestTokens("user5"));
+        toggleLike(postId2, generateTestTokens("user6"));
+
+
+        PostListAndPagingResponse response = RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .queryParam("boardId", boardId)
+                .queryParam("sortBy", SortBy.MOST_LIKED)
+                .queryParam("direction", Direction.ASC)
+                .when()
+                .get("/posts")
+                .then().log().all()
+                .statusCode(200)
+                .extract()
+                .as(PostListAndPagingResponse.class);
+
+        List<PostListResponse> postListResponses = response.postListResponse();
+
+        assertThat(postListResponses.get(0).postTitle()).isEqualTo("제목2");
+        assertThat(postListResponses.get(1).postTitle()).isEqualTo("제목3");
+        assertThat(postListResponses.get(2).postTitle()).isEqualTo("제목8");
+        assertThat(postListResponses.get(3).postTitle()).isEqualTo("제목1");
+        assertThat(postListResponses.get(4).postTitle()).isEqualTo("제목4");
+        assertThat(postListResponses.get(5).postTitle()).isEqualTo("제목5");
+
+    }
+
+    @DisplayName("사용자가 좋아요 누른 게시글 목록 보기")
+    @Test
+    public void 사용자가_좋아요_누른_게시글_목록_보기 (){
+
+        BoardResponse board = createBoard("자유게시판");
+        Long boardId = board.id();
+
+        PostResponse post1 = createPost(new CreatePostRequest(boardId, "제목1", "내용1", List.of("img1", "img2", "img3")));
+        PostResponse post2 = createPost(new CreatePostRequest(boardId, "제목2", "내용2", List.of("img1", "img2", "img3")));
+        PostResponse post3 = createPost(new CreatePostRequest(boardId, "제목3", "내용3", List.of("img1", "img2", "img3")));
+        PostResponse post4 = createPost(new CreatePostRequest(boardId, "제목4", "내용4", List.of("img1", "img2", "img3")));
+        PostResponse post5 = createPost(new CreatePostRequest(boardId, "제목5", "내용5", List.of("img1", "img2", "img3")));
+        PostResponse post6 = createPost(new CreatePostRequest(boardId, "제목6", "내용6", List.of("img1", "img2", "img3")));
+        PostResponse post7 = createPost(new CreatePostRequest(boardId, "제목7", "내용7", List.of("img1", "img2", "img3")));
+        PostResponse post8 = createPost(new CreatePostRequest(boardId, "제목8", "내용8", List.of("img1", "img2", "img3")));
+
+        Long postId8 = post8.id();
+        Long postId2 = post2.id();
+        Long postId3 = post3.id();
+
+        toggleLike(postId8, generateTestTokens("user6"));
+        toggleLike(postId2, generateTestTokens("user6"));
+        toggleLike(postId3, generateTestTokens("user6"));
+
+        String token = generateTestTokens("user6");
+
+        LikedPostListAndPagingResponse likedPosts = RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .header("Authorization", "Bearer " + token)
+                .when()
+                .get("/boards/liked")
+                .then().log().all()
+                .statusCode(200)
+                .extract()
+                .as(LikedPostListAndPagingResponse.class);
+
+        List<LikedPostListResponse> responses = likedPosts.likedPosts();
+
+        assertThat(responses.size()).isEqualTo(3);
+        assertThat(responses.get(0).postTitle()).isEqualTo("제목8");
+        assertThat(responses.get(1).postTitle()).isEqualTo("제목3");
+        assertThat(responses.get(2).postTitle()).isEqualTo("제목2");
+
+    }
 
 
 
@@ -374,7 +637,36 @@ public class BoardApiTest extends AcceptanceTest {
                 .as(DeletePostResponse.class);
     }
 
+    public PostLikeResponse toggleLike(Long postId, String token){
+
+        return RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .pathParam("postId", postId)
+                .header("Authorization", "Bearer " + token)
+                .when()
+                .post("/posts/{postId}/like")
+                .then().log().all()
+                .statusCode(200)
+                .extract()
+                .as(PostLikeResponse.class);
+    }
+
+
+
     public String generateTestToken(){
         return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwiaWF0IjoxNzQ1NDUyODAwLCJleHAiOjE3NzY5ODg4MDB9.P4f4xRaylLo8QXIqDxW8dFlLAEITtJr-hep4Ohyh42U";
+    }
+
+    private Map<String, String> tokens = Map.of(
+            "user1", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwiaWF0IjoxNzQ1NDUyODAwLCJleHAiOjE3NzY5ODg4MDB9.P4f4xRaylLo8QXIqDxW8dFlLAEITtJr-hep4Ohyh42U",
+            "user2", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIyMzQ1Njc4OTAxIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNzQ1ODM5MjAwLCJleHAiOjE3NzczNzUyMDB9.w7PfX7l9nZnK4VX2vAWoWE93_PXQpoYGlURuDzWjd_M",
+            "user3", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIzNDU2Nzg5MDEyIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNzQ1ODM5MjAwLCJleHAiOjE3NzczNzUyMDB9.ciysHp4jkmva9rXmhQ_E99Uw-3dGfMhsWblTwEzmVpQ",
+            "user4", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI0NTY3ODkwMTIzIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNzQ1ODM5MjAwLCJleHAiOjE3NzczNzUyMDB9.SQgt9bGWKm7x72RiyzbcrWVZNek5KTkQ48gXnNF7k74",
+            "user5", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1Njc4OTAxMjM0IiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNzQ1ODM5MjAwLCJleHAiOjE3NzczNzUyMDB9.HRAWZGvX_iETUKrd9FWBuP_3OE3k6Iiey4_naENWbsw",
+            "user6", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2Nzg5MDEyMzQ1IiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNzQ1ODM5MjAwLCJleHAiOjE3NzczNzUyMDB9.hnn1LeD1f9pZ8KWMqlPkUmugJmcwSzjEtfP--ht9Fn0"
+    );
+
+    public String generateTestTokens(String userId){
+        return tokens.get(userId);
     }
 }
