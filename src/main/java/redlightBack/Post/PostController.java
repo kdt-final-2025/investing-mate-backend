0307e6b7b297f72f6534c7f9f67ed5e25a9f81eb
HYOJIN <hjhwang08@naver.com>
2025-05-02 17:43:53 +0900

    PostLIke 게시물에 좋아요/좋아요 취소 구현 후 정렬 옵션에 반영, 좋아요한 게시물 목록 보기 구현 (#12)
    * feat: Post 좋아요 구현
    
    * feat: 사용자가 좋아요 한 post 목록 보기 구현
    
    * test: 좋아요/좋아요 취소, 사용자가 좋아요 한 post 목록 보기 테스트 & 통과
    
    * fix: Post Entity 필드값 null값 방지
    
    * fix: post 목록 보기 시 post 생성 날짜 추가
    
    * fix: lombok 수정
    
    * feat: 생성, 수정, 삭제 시 사용자 검증 로직 추가
    
    * test: 테스트에 사용자 검증 추가 & 통과
    
    * refactor: 좋아요/좋아요 취소 & 좋아요 수 집계 로직 분리
    
    * fix: 좋아요/좋아요 취소 controller 수정
    
    * feat: post 목록 보기 시 좋아요 수 반환
    
    * fix: Dto 이름 수정
    
    * fix: 좋아요 한 post 목록보기 dsl 수정
    
    * fix: likeCount 타입 long으로 변경
    
    * fix: isLiked true/false 전환 로직 수정
    
    * feat: Board 목록 보기 시 post 수 반환
    
    * fix: post와 postLike 관계 수정, null 방지
    
    * fix: 주석 추가, import 정리, 안 쓰는 controller 삭제
    
    * fix: post 수정, 삭제 시 작성자 검증 로직 수정
    
    * fix: 좋아요 한 목록보기 url 수정
    
    * test: 작성자 검증 로직, 좋아요 목록보기 수정 사항 테스트 반영 & 통과
    
    * fix: 안 쓰는 필드값, import 삭제

--- src/main/java/redlightBack/Post/PostController.java
+++ src/main/java/redlightBack/Post/PostController.java
@@ -1,6 +1,7 @@
 package redlightBack.Post;
 
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
@@ -9,15 +10,14 @@ import redlightBack.Post.Enum.Direction;
 import redlightBack.Post.Enum.SortBy;
 import redlightBack.loginUtils.LoginMemberId;
 
+@RequiredArgsConstructor
 @RestController
 public class PostController {
 
     private final PostService postService;
+    private final PostLikeService postLikeService;
 
-    public PostController(PostService postService) {
-        this.postService = postService;
-    }
-
+    //게시물 생성
     @PostMapping("/posts")
     public PostResponse createPost (@LoginMemberId String userId,
                                     @Valid @RequestBody CreatePostRequest request){
@@ -25,11 +25,13 @@ public class PostController {
         return postService.create(userId, request);
     }
 
+    //게시물 상세보기
     @GetMapping("/posts/{postId}")
     public PostResponse getPost (@PathVariable Long postId){
         return postService.getDetailPost(postId);
     }
 
+    //게시물 수정
     @PutMapping("posts/{postId}")
     public PostResponse updatePost (@LoginMemberId String userId,
                                     @PathVariable Long postId,
@@ -37,6 +39,7 @@ public class PostController {
         return postService.update(userId, postId, request);
     }
 
+    //게시물 삭제
     @DeleteMapping("posts/{postId}")
     public DeletePostResponse deletePost (@LoginMemberId String userId,
                                           @PathVariable Long postId){
@@ -44,6 +47,7 @@ public class PostController {
         return postService.delete(userId, postId);
     }
 
+    //게시물 목록보기
     @GetMapping("/posts")
     public PostListAndPagingResponse getPostList (
             @RequestParam Long boardId,
@@ -58,4 +62,26 @@ public class PostController {
 
         return postService.getPosts(userId, boardId, postTitle, sortBy, direction, pageable);
     }
+
+    //좋아요
+    @PostMapping("/posts/{postId}/like")
+    public PostLikeResponse like (@LoginMemberId String userId,
+                                  @PathVariable Long postId){
+
+
+        return postLikeService.toggleLike(userId, postId);
+    }
+
+    //좋아요 한 목록 보기
+    @GetMapping("/posts/liked")
+    public PostsLikedAndPagingResponse getLikedPostList (@LoginMemberId String userId,
+                                                         @RequestParam (defaultValue = "1") int pageNumber,
+                                                         @RequestParam (defaultValue = "10") int size){
+
+        Pageable pageable = PageRequest.of(pageNumber - 1, size);
+
+        return postService.likedPostList(userId, pageable);
+    }
+
+
 }
