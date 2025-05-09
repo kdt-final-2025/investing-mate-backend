cd11de30f649334c10ebb2134bff9d23d91d91f8
Yountaewoo <tty7797@naver.com>
2025-05-09 17:29:15 +0900

    feat : CI 설정 (#15)
    * feat : CI 설정
    
    * Update ci.yml
    
    * Update application.properties
    
    * update the CI workflow to explicitly handle Lombok
    
    * lombok 관련 dependency 추가
    
    * Update ci.yml
    
    * refactor: BaseEntity에 getter 직접 작성
    
    ---------
    
    Co-authored-by: Yul Jeon <dalbitstream@gmail.com>

--- src/main/java/redlightBack/common/BaseEntity.java
+++ src/main/java/redlightBack/common/BaseEntity.java
@@ -9,7 +9,6 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
-@Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
@@ -20,4 +19,11 @@ public abstract class BaseEntity {
     @LastModifiedDate
     private LocalDateTime updatedAt;
 
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
 }
