eda64364d99bcb04ab63821344f3a380441577ed
HYOJIN <hjhwang08@naver.com>
2025-05-12 16:02:35 +0900

    fix: 안 쓰는 dto, annotation 삭제

--- src/main/java/redlightBack/post/Post.java
+++ src/main/java/redlightBack/post/Post.java
@@ -9,7 +9,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-//@Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
--- src/main/java/redlightBack/stockRecommendation/dto/StockRecommendationDto.java
+++
@@ -1,9 +0,0 @@
-package redlightBack.stockRecommendation.dto;
-
-public record StockRecommendationDto(Long id,
-                                     String ticker,
-                                     String name,
-                                     Double currentPrice,
-                                     Double highPrice1y,
-                                     Double dividendYield) {
-}
